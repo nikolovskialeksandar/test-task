335f2c9e685da87ab40cf1a7fe9fc9389280e598
segasunset95 <nikolovskialexxandar@gmail.com>
2021-04-14 20:09:51 +0200

    Add logo

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,12 +4,18 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css'; 
 import Users from '../Users/Users';
 import Repos from '../Repos/Repos';
+import { Fragment } from 'react';
 
 const App = (props) => (
-  <Switch>
-    <Route path="/repos/" component={Repos} />
-    <Route path="/" component={Users} />
-  </Switch>
+  <Fragment>
+    <header>
+      <h1>GitXplore</h1>
+    </header>
+    <Switch>
+      <Route path="/repos/" component={Repos} />
+      <Route path="/" component={Users} />
+    </Switch>
+  </Fragment>
 );
 
 const mapStateToProps = (state) => ({
